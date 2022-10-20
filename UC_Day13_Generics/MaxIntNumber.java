c2f2d98621ea91aa9aeb58e611b5d6eb069f54ba
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-10-21 02:16:22 +0530

    Added: Find Max float among 3 num logic

--- UC_Day13_Generics/MaxIntNumber.java
+++ UC_Day13_Generics/MaxIntNumber.java
@@ -1,9 +1,9 @@
 package com.bridgelabz.day13.UC_Day13_Generics;
 
 public class MaxIntNumber {
-	public static Integer maxNumberInteger(Integer num1, Integer num2, Integer num3)
+	public static Float maxNumberFloat(Float num1, Float num2, Float num3)
 	{
-		Integer max = num1;
+		Float max = num1;
 		if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
 			max = num1;
 		else if(num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
@@ -14,7 +14,7 @@ public class MaxIntNumber {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("The max Number Among 3 numbers is: "+maxNumberInteger(93, 26, 43));
+		System.out.println("The max Number Among 3 numbers is: "+maxNumberFloat(93.32f, 2698.76f, 437.3f));
 
 	}
 
