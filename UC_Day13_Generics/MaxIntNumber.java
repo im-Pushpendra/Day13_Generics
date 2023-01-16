29b3da5faff30561cf4db37909b81fa4859189da
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2023-01-16 18:34:48 +0530

    Refactor1

--- UC_Day13_Generics/MaxIntNumber.java
+++ UC_Day13_Generics/MaxIntNumber.java
@@ -1,9 +1,10 @@
 package com.bridgelabz.day13.UC_Day13_Generics;
 
-public class MaxIntNumber {
-	public static String maxStringNumber(String num1, String num2, String num3)
+public class MaxIntNumber<T> {
+	T num1, num2, num3;
+	public static <T extends Comparable<T>> T maxAmong3Number(T num1, T num2, T num3)
 	{
-		String max = num1;
+		T max = num1;
 		if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
 			max = num1;
 		else if(num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
@@ -12,11 +13,10 @@ public class MaxIntNumber {
 			max = num3;
 		return max;
 	}
-
 	public static void main(String[] args) {
-		System.out.println("The max Number Among 3 numbers is: "+maxStringNumber("Apple", "Peach", "Banana"));
-
-
+		System.out.println("The max Number Among 3 String numbers is: "+maxAmong3Number("Apple", "Dragonfruit", "Banana"));
+		System.out.println("The max Number Among 3 Integer numbers is: "+maxAmong3Number(1504,22,27));
+		System.out.println("The max Number Among 3 Float numbers is: "+maxAmong3Number(32.5f,56.98f,78.99f));
 	}
 
 }
